9d316f6afe316d373b60e144bf8f6a65b5dcd1c6
Chloe Sheasby <chloe.sheasby@eagles.oc.edu>
2022-01-26 14:26:55 -0600

    edited the server.js file to include routes

--- server.js
+++ server.js
@@ -25,7 +25,18 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to tutoring node application." });
 });
 
+require("./app/routes/appointment.routes")(app);
+require("./app/routes/availability.routes")(app);
+require("./app/routes/group.routes")(app);
+require("./app/routes/location.routes")(app);
 require("./app/routes/person.routes")(app);
+require("./app/routes/personappointment.routes")(app);
+require("./app/routes/personrole.routes")(app);
+require("./app/routes/persontopic.routes")(app);
+require("./app/routes/request.routes")(app);
+require("./app/routes/role.routes")(app);
+require("./app/routes/session.routes")(app);
+require("./app/routes/topic.routes")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
