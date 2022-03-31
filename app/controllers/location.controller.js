02bf80f0d643e8e885188ee5ce48c7c026afae17
Chloe Sheasby <chloe.sheasby@eagles.oc.edu>
2022-03-31 16:30:30 -0500

    added some functions to help filtering by group

--- app/controllers/location.controller.js
+++ app/controllers/location.controller.js
@@ -52,6 +52,22 @@ exports.findAll = (req, res) => {
       });
   };
 
+  // Retrieve all locations for a group from the database.
+exports.findAllForGroup = (req, res) => {
+  const id = req.params.groupId;
+
+  Location.findAll({ where: {groupId: id} })
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving locations for group."
+      });
+    });
+};
+
 // Find a single Location with an id
 exports.findOne = (req, res) => {
     const id = req.params.id;
