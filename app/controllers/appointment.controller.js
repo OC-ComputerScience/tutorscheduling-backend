5850a5109caf8a99098cc8f935defc70f9df04a5
Chloe Sheasby <47607144+ChloeSheasby@users.noreply.github.com>
2022-04-26 13:10:08 -0500

    Update appointment.controller.js
    quick async add

--- app/controllers/appointment.controller.js
+++ app/controllers/appointment.controller.js
@@ -511,7 +511,7 @@ async function findFirstTutorForAppointment(id) {
  * @param {Object} credentials The authorization client credentials.
  * @param {function} callback The callback to call with the authorized client.
  */
- function authorize(credentials, callback, data) {
+ async function authorize(credentials, callback, data) {
   const {client_secret, client_id, redirect_uris} = credentials.web;
   const oAuth2Client = new google.auth.OAuth2(
       client_id, client_secret, redirect_uris[0]);
