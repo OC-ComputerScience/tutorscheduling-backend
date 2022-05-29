9713bc18acd4e341a89ca72d47129d158512e8cf
Grace Hopper <ghopp01@gmail.com>
2022-05-29 15:09:39 -0500

    Provide routes and controllers for personappointments
    added some controllers and routes to help with retrieving appointments for appointment feedback by person

--- app/routes/personappointment.routes.js
+++ app/routes/personappointment.routes.js
@@ -11,6 +11,12 @@ module.exports = app => {
     // Retrieve all PersonAppointment
     router.get("/", personappointment.findAll);
   
+    // Retrieve a single PersonAppointment with role and person
+    router.get("/person/:personId/appointment/:appointmentId", personappointment.findOneForType);
+
+    // Retrieve personappointments for a specific person
+    router.get("/person/:personId", personappointment.findAllForPerson);
+
     // Retrieve a single PersonAppointment with id
     router.get("/:id", personappointment.findOne);
   
