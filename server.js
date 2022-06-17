f1a08b9dfbe998db7c55a0ea8241d7a8f3d8b50d
David North <david.north@oc.edu>
2022-06-17 06:46:58 -0500

    Change route / message

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to tutoring node application." });
+  res.json({ message: "Welcome to tutoring node application!" });
 });
 
 require("./app/routes/appointment.routes")(app);
