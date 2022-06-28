7af4264c8c22b7d6d2ed7b9f1db74bc78b411308
Joshua Murray <joshua.murray@eagles.oc.edu>
2022-06-28 15:22:13 -0500

    updates for session notifications

--- app/background/hourly.js
+++ app/background/hourly.js
@@ -2,7 +2,14 @@ const cron = require('node-cron');
 const db = require("../models");
 const Appointment = db.appointment;
 const PersonAppointment = db.personappointment;
+const Person = db.person;
+const sms = require("../controllers/twilio.controller.js");
 const Op = db.Sequelize.Op;
+const accountSid = process.env.TWILIO_ACCOUNT_SID; 
+const authToken = process.env.TWILIO_AUTH_TOKEN1 + process.env.TWILIO_AUTH_TOKEN2;
+const phoneNum = process.env.TWILIO_NUMBER;
+const client = require('twilio')(accountSid, authToken);
+
 
 // Schedule tasks to be run on the server 12:01 am.
 // From : https://www.digitalocean.com/community/tutorials/nodejs-cron-jobs-by-examples
@@ -14,8 +21,9 @@ const Op = db.Sequelize.Op;
 //      cron.schedule('* * * * *', function() {
         console.log('Scheduled task every day at 55 min past the hour');
         deletePastAppointments();
+        notifyUpcomingAppointments();
         })
-}
+  } 
 
   async function deletePastAppointments() {
     let delDate = new Date().setHours(0,0,0);
@@ -79,6 +87,104 @@ const Op = db.Sequelize.Op;
         console.log("Could not delete past PersonAppointments"+ err);
         });
          
+  }
+
+  async function notifyUpcomingAppointments() {
+    let date = new Date().setHours(0,0,0);
+    let startDate = new Date();
+    startDate.setHours(startDate.getHours() + 1);
+    let startTime = startDate.toLocaleTimeString('it-IT');
+    let endDate = new Date();
+    endDate.setHours(endDate.getHours() + 2);
+    let endTime = endDate.toLocaleTimeString('it-IT');
+    let personAppointments = [];
+    //get all of the appointments for today that start before now
+    await PersonAppointment.findAll({
+      include: [{
+        model: Appointment,
+        as: 'appointment',
+        where: {
+          status: 'booked',
+          date : {[Op.eq]: date},
+          startTime : { [Op.and]: [ 
+            {[Op.gt]: startTime},
+            {[Op.lt]: endTime}]
+          },
+        },
+        required: true
+      }]
+    })
+      .then(pap => {
+        console.log(pap.length+" people found with booked appointments");
+        if (pap.length >0) {
+          pap.forEach((pa) => {
+            personAppointments.push(pa);
+          })
+        }
+        PersonAppointment.findAll({
+          as: 'personAppointment',
+          where: {
+            [Op.or]: [
+              {isTutor: false},
+              {appointmentId : {[Op.in]:  db.sequelize.literal("(SELECT appointmentId FROM personappointments where appointmentId=personAppointment.appointmentId AND isTutor='0')")},
+            }
+            ]
+          },
+          include: [{
+            model: Appointment,
+            as: 'appointment',
+            where: {
+              status: 'available',
+              type: 'Group',
+              date : {[Op.eq]: date},
+              startTime : { [Op.and]: [ 
+                {[Op.gt]: startTime},
+                {[Op.lt]: endTime}]
+              },
+            },
+            required: true
+          }]
+        })
+        .then((morePap)=> {
+          console.log(morePap.length+" people found with group appointments");
+          if (morePap.length >0) {
+            morePap.forEach((pa) => {
+              personAppointments.push(pa);
+            })
+          }
+          personAppointments.forEach((personAppoint) => {
+            Person.findByPk(personAppoint.personId).then((person) => {
+              Appointment.findByPk(personAppoint.appointmentId).then((appoint) => {
+                let message = {
+                  message: "You have an upcoming tutoring appointment:\n    Type: " + appoint.type + "\n    Time: " + appoint.startTime + "\n    Location: " + appoint.locationId,
+                  phoneNum: person.phoneNum,
+                }
+                client.messages
+                  .create({
+                    body: message.message,
+                    from: phoneNum,
+                    to: message.phoneNum
+                  })
+                  .then(message => console.log("sent" + message.sid))
+                  .catch(err => {
+                    console.log("Could not send messsage"+ err);
+                  });
+
+              })
+            })
+
+            })
+          })
+          .catch(err => {
+            console.log("Could not work"+ err);
+          })
+        })
+        .catch(err => {
+          console.log("Could not work 2"+ err);
+        })
+      .catch(err => {
+        console.log("Could not work 3"+ err);
+      });
   }
 
 
