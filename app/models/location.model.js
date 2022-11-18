80b10715ba34978bda00dcb61e501a59a11cb90f
Chloe Sheasby <chloe.sheasby@eagles.oc.edu>
2022-11-18 15:47:13 -0600

    some formatting fixes

--- app/models/location.model.js
+++ app/models/location.model.js
@@ -1,30 +1,30 @@
 module.exports = (sequelize, Sequelize) => {
-    const Location = sequelize.define("location", {
-      id: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        primaryKey: true
-      },
-      name: {
-        type: Sequelize.STRING,
-        allowNull: false
-      },
-      type: {
-        type: Sequelize.STRING,
-        allowNull: false
-      },
-      building: {
-        type: Sequelize.STRING,
-        allowNull: false
-      },
-      description: {
-        type: Sequelize.STRING
-      },
-      status: {
-        type: Sequelize.STRING,
-        allowNull: false
-      }
-    });
-  
-    return Location;
+  const Location = sequelize.define("location", {
+    id: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    type: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    building: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: Sequelize.STRING,
+    },
+    status: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+  });
+
+  return Location;
 };
