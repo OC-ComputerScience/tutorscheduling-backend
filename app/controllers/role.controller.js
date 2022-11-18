ad1bbb1e2bfa5528a18cb833bce155376ed37f97
Chloe Sheasby <chloe.sheasby@eagles.oc.edu>
2022-11-18 15:51:26 -0600

    fixed some formatting

--- app/controllers/role.controller.js
+++ app/controllers/role.controller.js
@@ -7,63 +7,62 @@ const Op = db.Sequelize.Op;
 
 // Create and Save a new Role
 exports.create = (req, res) => {
-    // Validate request
-    if (!req.body.type) {
-      res.status(400).send({
-        message: "Content can not be empty!"
-      });
-      return;
-    }
-  
-    // Create a Role
-    const role = {
-      id: req.body.id,
-      groupId: req.body.groupId,
-      type: req.body.type
-    };
-  
-    // Save Role in the database
-    Role.create(role)
-      .then(data => {
-        res.send(data);
-      })
-      .catch(err => {
-        res.status(500).send({
-          message:
-            err.message || "Some error occurred while creating the Role."
-        });
-      });
+  // Validate request
+  if (!req.body.type) {
+    res.status(400).send({
+      message: "Content can not be empty!",
+    });
+    return;
+  }
+
+  // Create a Role
+  const role = {
+    id: req.body.id,
+    groupId: req.body.groupId,
+    type: req.body.type,
   };
+
+  // Save Role in the database
+  Role.create(role)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Some error occurred while creating the Role.",
+      });
+    });
+};
 
 // Retrieve all Roles from the database.
 exports.findAll = (req, res) => {
-    const id = req.query.id;
-    var condition = id ? { id: { [Op.like]: `%${id}%` } } : null;
-  
-    Role.findAll({ where: condition, include: ["group"]  })
-      .then(data => {
-        res.send(data);
-      })
-      .catch(err => {
-        res.status(500).send({
-          message:
-            err.message || "Some error occurred while retrieving roles."
-        });
+  const id = req.query.id;
+  var condition = id ? { id: { [Op.like]: `%${id}%` } } : null;
+
+  Role.findAll({ where: condition, include: ["group"] })
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Some error occurred while retrieving roles.",
       });
-  };
+    });
+};
 
 // Retrieve all Roles for a group from the database.
 exports.findAllForGroup = (req, res) => {
   const id = req.params.groupId;
 
-  Role.findAll({ where: {groupId: id} })
-    .then(data => {
+  Role.findAll({ where: { groupId: id } })
+    .then((data) => {
       res.send(data);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).send({
         message:
-          err.message || "Some error occurred while retrieving roles for group."
+          err.message ||
+          "Some error occurred while retrieving roles for group.",
       });
     });
 };
@@ -74,25 +73,28 @@ exports.findRoleByGroupForPerson = (req, res) => {
   const groupId = req.params.groupId;
 
   Role.findAll({
-    where: { '$personrole.personId$': personId, groupId: groupId },
-    include: [ {
-        model: PersonRole, 
-        as: 'personrole',
+    where: { "$personrole.personId$": personId, groupId: groupId },
+    include: [
+      {
+        model: PersonRole,
+        as: "personrole",
         right: true,
-        include: [ {
-          model: PersonRolePrivilege,
-          as: 'personroleprivilege'
-        } ]
-    } ]
+        include: [
+          {
+            model: PersonRolePrivilege,
+            as: "personroleprivilege",
+          },
+        ],
+      },
+    ],
   })
-  .then((data) => {
+    .then((data) => {
       res.send(data);
-  })
-  .catch(err => {
+    })
+    .catch((err) => {
       res.status(500).send({ message: err.message });
-  });
-
-}
+    });
+};
 
 // Retrieve roles per group for personroles for a specific type
 exports.findRoleByGroupForType = (req, res) => {
@@ -101,157 +103,160 @@ exports.findRoleByGroupForType = (req, res) => {
 
   Role.findAll({
     where: { type: type, groupId: groupId },
-    include: [ {
-        model: PersonRole, 
-        as: 'personrole',
+    include: [
+      {
+        model: PersonRole,
+        as: "personrole",
         right: true,
-        include: [{
+        include: [
+          {
             model: PersonRolePrivilege,
-            as: 'personroleprivilege'
+            as: "personroleprivilege",
           },
           {
             model: Person,
-            as: 'person',
-            right: true, 
-        }]
-    }]
+            as: "person",
+            right: true,
+          },
+        ],
+      },
+    ],
   })
-  .then((data) => {
+    .then((data) => {
       res.send(data);
-  })
-  .catch(err => {
+    })
+    .catch((err) => {
       res.status(500).send({ message: err.message });
-  });
-
-}
+    });
+};
 
 // Retrieve a role for a personrole for a specific person
 exports.findRoleForPerson = (req, res) => {
   const id = req.params.personId;
 
   Role.findAll({
-    where: { '$personrole.personId$': id },
-    include: [ {
-        model: PersonRole, 
-        as: 'personrole',
-        right: true
-    } ]
+    where: { "$personrole.personId$": id },
+    include: [
+      {
+        model: PersonRole,
+        as: "personrole",
+        right: true,
+      },
+    ],
   })
-  .then((data) => {
+    .then((data) => {
       res.send(data);
-  })
-  .catch(err => {
+    })
+    .catch((err) => {
       res.status(500).send({ message: err.message });
-  });
-
-}
+    });
+};
 
 // Retrieve a role for a personrole for a specific person
 exports.findIncompleteRoleForPerson = (req, res) => {
   const id = req.params.personId;
 
   Role.findAll({
-    where: { '$personrole.personId$': id, '$personrole.agree$': false },
-    include: [ {
-        model: PersonRole, 
-        as: 'personrole',
-        right: true
-    } ]
+    where: { "$personrole.personId$": id, "$personrole.agree$": false },
+    include: [
+      {
+        model: PersonRole,
+        as: "personrole",
+        right: true,
+      },
+    ],
   })
-  .then((data) => {
+    .then((data) => {
       res.send(data);
-  })
-  .catch(err => {
+    })
+    .catch((err) => {
       res.status(500).send({ message: err.message });
-  });
-
-}
+    });
+};
 
 // Find a single Role with an id
 exports.findOne = (req, res) => {
-    const id = req.params.id;
-  
-    Role.findByPk(id)
-      .then(data => {
-        if (data) {
-          res.send(data);
-        } else {
-          res.status(404).send({
-            message: `Cannot find Role with id=${id}.`
-          });
-        }
-      })
-      .catch(err => {
-        res.status(500).send({
-          message: "Error retrieving Role with id=" + id
+  const id = req.params.id;
+
+  Role.findByPk(id)
+    .then((data) => {
+      if (data) {
+        res.send(data);
+      } else {
+        res.status(404).send({
+          message: `Cannot find Role with id=${id}.`,
         });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Error retrieving Role with id=" + id,
       });
-  };
+    });
+};
 
 // Update a Role by the id in the request
 exports.update = (req, res) => {
-    const id = req.params.id;
-  
-    Role.update(req.body, {
-      where: { id: id }
-    })
-      .then(num => {
-        if (num == 1) {
-          res.send({
-            message: "Role was updated successfully."
-          });
-        } else {
-          res.send({
-            message: `Cannot update Role with id=${id}. Maybe Person was not found or req.body is empty!`
-          });
-        }
-      })
-      .catch(err => {
-        res.status(500).send({
-          message: "Error updating Role with id=" + id
+  const id = req.params.id;
+
+  Role.update(req.body, {
+    where: { id: id },
+  })
+    .then((num) => {
+      if (num == 1) {
+        res.send({
+          message: "Role was updated successfully.",
         });
+      } else {
+        res.send({
+          message: `Cannot update Role with id=${id}. Maybe Person was not found or req.body is empty!`,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Error updating Role with id=" + id,
       });
-  };
+    });
+};
 
 // Delete a Role with the specified id in the request
 exports.delete = (req, res) => {
-    const id = req.params.id;
-  
-    Role.destroy({
-      where: { id: id }
-    })
-      .then(num => {
-        if (num == 1) {
-          res.send({
-            message: "Role was deleted successfully!"
-          });
-        } else {
-          res.send({
-            message: `Cannot delete Role with id=${id}. Maybe Role was not found!`
-          });
-        }
-      })
-      .catch(err => {
-        res.status(500).send({
-          message: "Could not delete Role with id=" + id
+  const id = req.params.id;
+
+  Role.destroy({
+    where: { id: id },
+  })
+    .then((num) => {
+      if (num == 1) {
+        res.send({
+          message: "Role was deleted successfully!",
         });
+      } else {
+        res.send({
+          message: `Cannot delete Role with id=${id}. Maybe Role was not found!`,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Could not delete Role with id=" + id,
       });
-  };
+    });
+};
 
 // Delete all Roles from the database.
 exports.deleteAll = (req, res) => {
-    Role.destroy({
-      where: {},
-      truncate: false
+  Role.destroy({
+    where: {},
+    truncate: false,
+  })
+    .then((nums) => {
+      res.send({ message: `${nums} Roles were deleted successfully!` });
     })
-      .then(nums => {
-        res.send({ message: `${nums} Roles were deleted successfully!` });
-      })
-      .catch(err => {
-        res.status(500).send({
-          message:
-            err.message || "Some error occurred while removing all roles."
-        });
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Some error occurred while removing all roles.",
       });
-  };
-  
+    });
+};
--- app/routes/role.routes.js
+++ app/routes/role.routes.js
@@ -1,42 +1,57 @@
-module.exports = app => {
-    const role = require("../controllers/role.controller.js");
-    const { authenticate,isAdmin,isSuperAdmin} = require("../authorization/authorization.js");
-  
-    var router = require("express").Router();
-  
-    // Create a new Role
-    router.post("/", [authenticate,isSuperAdmin],role.create);
-  
-    // Retrieve all Role
-    router.get("/", [authenticate],role.findAll);
-  
-    // Retrieve a single Role with id
-    router.get("/:id", [authenticate],role.findOne);
+module.exports = (app) => {
+  const role = require("../controllers/role.controller.js");
+  const {
+    authenticate,
+    isAdmin,
+    isSuperAdmin,
+  } = require("../authorization/authorization.js");
 
-    // Retrieve roles for a specific group
-    router.get("/group/:groupId", [authenticate],role.findAllForGroup);
+  var router = require("express").Router();
 
-    // Retrieve roles for a specific person including personrole
-    router.get("/person/:personId", [authenticate],role.findRoleForPerson);
+  // Create a new Role
+  router.post("/", [authenticate, isSuperAdmin], role.create);
 
-    // Retrieve roles by group for a specific type
-    router.get("/group/:groupId/type/:type", [authenticate],role.findRoleByGroupForType);
+  // Retrieve all Role
+  router.get("/", [authenticate], role.findAll);
 
-    // Retrieve roles by group for a specific person including personroles
-    router.get("/group/:groupId/person/:personId", [authenticate],role.findRoleByGroupForPerson);
+  // Retrieve a single Role with id
+  router.get("/:id", [authenticate], role.findOne);
 
-    // Retrieve incomplete roles for a specific person including personrole
-    router.get("/personIn/:personId", [authenticate],role.findIncompleteRoleForPerson);
-  
-    // Update a Role with id
-    router.put("/:id", [authenticate,isSuperAdmin], role.update);
-  
-    // Delete a Role with id
-    router.delete("/:id", [authenticate,isSuperAdmin],role.delete);
-  
-    // Delete all Roles
-    router.delete("/", [authenticate,isSuperAdmin],role.deleteAll);
-  
-    app.use('/role', router);
-  };
-  
+  // Retrieve roles for a specific group
+  router.get("/group/:groupId", [authenticate], role.findAllForGroup);
+
+  // Retrieve roles for a specific person including personrole
+  router.get("/person/:personId", [authenticate], role.findRoleForPerson);
+
+  // Retrieve roles by group for a specific type
+  router.get(
+    "/group/:groupId/type/:type",
+    [authenticate],
+    role.findRoleByGroupForType
+  );
+
+  // Retrieve roles by group for a specific person including personroles
+  router.get(
+    "/group/:groupId/person/:personId",
+    [authenticate],
+    role.findRoleByGroupForPerson
+  );
+
+  // Retrieve incomplete roles for a specific person including personrole
+  router.get(
+    "/personIn/:personId",
+    [authenticate],
+    role.findIncompleteRoleForPerson
+  );
+
+  // Update a Role with id
+  router.put("/:id", [authenticate, isSuperAdmin], role.update);
+
+  // Delete a Role with id
+  router.delete("/:id", [authenticate, isSuperAdmin], role.delete);
+
+  // Delete all Roles
+  router.delete("/", [authenticate, isSuperAdmin], role.deleteAll);
+
+  app.use("/role", router);
+};
