0da1c4bcb90e859231536d15ef655d3879244282
Chloe Sheasby <chloesheasby@gmail.com>
2023-03-08 16:08:30 -0600

    removed unnecessary line

--- app/utils/twilio.js
+++ app/utils/twilio.js
@@ -1,5 +1,4 @@
 const Person = require("./person.js");
-const Time = require("./timeFunctions.js");
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken =
   process.env.TWILIO_AUTH_TOKEN1 + process.env.TWILIO_AUTH_TOKEN2;
